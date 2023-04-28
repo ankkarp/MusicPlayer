2645c45dcd38b7e9ffbf2ea2360fcfe873188514
ankkarp <anna.a.k.2002@gmail.com>
2023-04-28 10:42:01 +0300

    button for permission request

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -15,9 +15,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionRequired
@@ -107,9 +110,17 @@ fun MyContent() {
         }
     }
     else {
-        Text("Нет доступа к файловой системе.")
-        Button(onClick = { fsPermissionState.launchPermissionRequest() }) {
-            Text("Предоставить доступ к памяти устройства")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        )
+        {
+            Column {
+                Text("Нет доступа к файловой системе.")
+                Button(onClick = { fsPermissionState.launchPermissionRequest() }, modifier = Modifier.width(200.dp)) {
+                    Text("Предоставить доступ к памяти устройства")
+                }
+            }
         }
     }
 }
